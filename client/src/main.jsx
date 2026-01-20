94b14b52307a381a378e80653e37e1a2ec2fdaba
Prashaxt <147325777+Prashaxt@users.noreply.github.com>
2026-01-20 12:30:11 +0530

    Remove GitHub Pages basename for Vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/PinHoader-client">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
